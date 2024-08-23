6b1cd928e17dbf860a7456f37ff55977b5d10238
Armine Muradyan <muradyan.armin13@gmail.com>
2024-08-23 15:35:48 +0400

    add useNavigate hook

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,15 +1,28 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
+import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.courseSlug);
+  // simply show notfound component
+  //   if (!course) {
+  //     return <NotFound />;
+  //   }
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course]);
+
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
-      {/* <Link to="/courses">Back All Courses</Link> */}
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
+      {/* <Link to="/courses">Back All Courses</Link>  misht hnaravorins ogtagorcenq haraberakan chanaparhner*/}
       <Link to=".." relative="path">
         Back All Courses
       </Link>
