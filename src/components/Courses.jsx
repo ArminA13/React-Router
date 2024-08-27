61e297348d32d37fa93034cbefd641ab39d873e7
Armine Muradyan <muradyan.armin13@gmail.com>
2024-08-27 16:07:46 +0400

    add new changis for sorting courses

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,24 +1,34 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import courses from "../data/courses";
+
+const SORT_KEYS = ["title", "slug", "id"];
 
 function sortCourses(courses, key) {
   const sortedCourses = [...courses];
-  if (!key) {
-    return sortCourses;
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses;
   }
   sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
   return sortedCourses;
 }
 
 const Courses = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const query = queryString.parse(location.search);
   const [sortKey, setSortKey] = useState(query.sort);
   const [sortedCourses, setSortedCourses] = useState(
     sortCourses(courses, sortKey)
   );
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate(".");
+    }
+    setSortKey();
+    setSortedCourses([...courses]);
+  }, [sortKey, navigate]);
 
   return (
     <>
